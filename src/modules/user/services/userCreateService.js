203f2cb76c8de2ed66bc29b069fa1919577c9ad0
Adryann Millos Santos de Freitas <adryann.freitas@ubistart.com>
2023-05-18 14:33:25 -0300

    feat final commit

--- src/modules/user/services/userCreateService.js
+++ src/modules/user/services/userCreateService.js
@@ -1,10 +1,12 @@
 const userRepository = require("../repositories/userRepository");
+const bcrypt = require("bcrypt");
 
 async function execute(user) {
     const userFound = await userRepository.findByEmail(user.email);
     if (userFound) {
         throw new Error("User already exists");
     }
+    user.password = bcrypt.hashSync(user.password, 10);
     const newUser = await userRepository.create(user);
     return newUser;
 }
--- src/server.js
+++ src/server.js
@@ -2,6 +2,6 @@
 const app = require("./app");
 require("dotenv").config();
 
-const PORT = process.env.PORT || 8009;
+const PORT = process.env.PORT || 8888;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
